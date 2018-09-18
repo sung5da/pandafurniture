23297551e503e241069db8fc0564c28b7fc28e97
smpk32 <43022845+smpk32@users.noreply.github.com>
2018-09-18 14:37:18 +0900

    수정되
    열었당심히 합시당^^

--- bestfood/android/app/src/main/java/com/example/psm/bestfood/IndexActivity.java
+++ bestfood/android/app/src/main/java/com/example/psm/bestfood/IndexActivity.java
@@ -148,11 +148,7 @@ public class IndexActivity extends AppCompatActivity {
                 }
             }
 
-            @Override
-            public void onFailure(Call<String> call, Throwable t) {
-                MyLog.d(TAG,"no internet connectivity");
 
-            }
         });
     }
 }
