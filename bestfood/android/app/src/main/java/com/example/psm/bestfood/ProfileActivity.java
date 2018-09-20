7c7fa31ee778e666fcac8c2cc5255de873f8bb2f
smpk32 <43022845+smpk32@users.noreply.github.com>
2018-09-20 11:36:30 +0900

    Revert "Revert "Merge branch 'sangmin's' into sung5""
    This reverts commit e8c8ca2ef9d939ff3ca7c3be40cdba072580ecc9.

---
+++ bestfood/android/app/src/main/java/com/example/psm/bestfood/ProfileActivity.java
@@ -0,0 +1,304 @@
+package com.example.psm.bestfood;
+
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RemoteViewsService;
+import android.widget.TextView;
+
+
+import com.example.psm.bestfood.item.MemberInfoItem;
+import com.example.psm.bestfood.lib.EtcLib;
+import com.example.psm.bestfood.lib.MyLog;
+import com.example.psm.bestfood.lib.MyToast;
+import com.example.psm.bestfood.lib.StringLib;
+import com.example.psm.bestfood.remote.*;
+import com.squareup.picasso.Picasso;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+//import com.example.psm.bestfood.remote.RemoteService;
+//import com.example.psm.bestfood.remote.ServiceGenerator;
+
+public class ProfileActivity extends AppCompatActivity implements View.OnClickListener{
+    private final String TAG = this.getClass().getSimpleName();
+    Context context;
+    ImageView profileIconImage;
+    ImageView profileIconChangeImage;
+    EditText nameEdit;
+    EditText sextypeEdit;
+    EditText birthEdit;
+    EditText phoneEdit;
+
+    MemberInfoItem currentItem;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        context = this;
+
+        currentItem = ((MyApp)getApplication()).getMemberInfoItem();
+
+        setToolbar();
+        setView();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        MyLog.d(TAG,RemoteService.MEMBER_ICON_URL + currentItem.memberIconFilename);
+
+        if(StringLib.getInstance().isBlank(currentItem.memberIconFilename)){
+            Picasso.with(this).load(R.drawable.ic_person).into(profileIconImage);
+        }else{
+            Picasso.with(this).load(RemoteService.MEMBER_ICON_URL + currentItem.memberIconFilename).into(profileIconImage);
+        }
+    }
+
+    private void setToolbar(){
+        final Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        final ActionBar actionBar = getSupportActionBar();
+
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle(R.string.profile_setting);
+        }
+    }
+
+    private void setView(){
+        profileIconImage = (ImageView)findViewById(R.id.profile_icon);
+        profileIconImage.setOnClickListener(this);
+
+        nameEdit = (EditText)findViewById(R.id.profile_name);
+        nameEdit.setText(currentItem.name);
+
+        sextypeEdit = (EditText)findViewById(R.id.profile_sextype);
+        sextypeEdit.setText(currentItem.sextype);
+        sextypeEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setSexTypeDialog();
+            }
+        });
+
+        birthEdit = (EditText)findViewById(R.id.profile_birth);
+        birthEdit.setText(currentItem.birthday);
+        birthEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setBirthdayDialog();
+            }
+        });
+
+        String phoneNumber = EtcLib.getInstance().getPhoneNumber(context);
+
+        phoneEdit = (EditText)findViewById(R.id.profile_phone);
+        phoneEdit.setText(currentItem.phone);
+
+        TextView phoneStateEdit = (TextView)findViewById(R.id.phone_state);
+        if(phoneNumber.startsWith("0")){
+            phoneStateEdit.setText("("+getResources().getString(R.string.device_number)+")");
+        }else {
+            phoneStateEdit.setText("("+getResources().getString(R.string.phone_number)+")");
+        }
+    }
+
+    private void setSexTypeDialog(){
+        final String[] sexTypes = new String[2];
+        sexTypes[0] = getResources().getString(R.string.sex_man);
+        sexTypes[1] = getResources().getString(R.string.sex_woman);
+
+        new AlertDialog.Builder(this).setItems(sexTypes, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if(which>=0){
+                    sextypeEdit.setText(sexTypes[which]);
+                }
+                dialog.dismiss();
+            }
+        }).show();
+    }
+
+    private void setBirthdayDialog(){
+        GregorianCalendar calendar = new GregorianCalendar();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        final int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                String myMonth;
+                if(monthOfYear + 1 <10){
+                    myMonth = "0"+(monthOfYear + 1);
+                }else{
+                    myMonth=""+(monthOfYear+1);
+                }
+
+                String myDay;
+                if(dayOfMonth<10){
+                    myDay = "0" + dayOfMonth;
+                }else {
+                    myDay = "" + dayOfMonth;
+                }
+
+                String date = year + " " + myMonth + " " + myDay;
+                birthEdit.setText(date);
+            }
+        },year, month, day).show();
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu){
+//        getMenuInflater().inflate(R.menu.menu_submit,menu);
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item){
+//        switch (item.getItemId()){
+//            case android.R.id.home:
+//                close();
+//                break;
+//
+//            case R.id.action_submit:
+//                save();
+//                break;
+//        }
+//
+//        return true;
+//    }
+
+    private MemberInfoItem getMemberInfoItem(){
+        MemberInfoItem item = new MemberInfoItem();
+        item.phone = EtcLib.getInstance().getPhoneNumber(context);
+        item.name = nameEdit.getText().toString();
+        item.sextype = sextypeEdit.getText().toString();
+        item.birthday = birthEdit.getText().toString().replace("","");
+
+        return item;
+    }
+
+    private boolean isChanged(MemberInfoItem newItem){
+        if(newItem.name.trim().equals(currentItem.name)&&newItem.sextype.trim().equals(currentItem.sextype)&&newItem.birthday.trim().equals(currentItem.birthday)){
+            Log.d(TAG,"return"+false);
+            return false;
+        }else{
+            return true;
+        }
+    }
+
+    private boolean isNoName(MemberInfoItem newItem){
+        if(StringLib.getInstance().isBlank(newItem.name)){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    private void close(){
+        MemberInfoItem newItem = getMemberInfoItem();
+
+        if(!isChanged(newItem)&&!isNoName(newItem)){
+            finish();
+        } else if(isNoName(newItem)){
+            MyToast.s(context,R.string.name_need);
+            finish();
+        }else{
+            new AlertDialog.Builder(this).setTitle(R.string.change_save).setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    save();
+                }
+            }).setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    finish();
+                }
+            }).show();
+        }
+    }
+
+    private void save(){
+        final MemberInfoItem newItem = getMemberInfoItem();
+
+        if(!isChanged(newItem)){
+            MyToast.s(this, R.string.no_change);
+            finish();
+            return;
+        }
+
+        MyLog.d(TAG, "insterItem"+newItem.toString());
+
+        RemoteService remoteService = ServiceGenerator.createService(RemoteService.class);
+
+        Call<String> call = remoteService.insertMemberInfo(newItem);
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                if (response.isSuccessful()){
+                    String seq = response.body();
+                    try{
+                        currentItem.seq = Integer.parseInt(seq);
+                        if(currentItem.seq == 0 ){
+                            MyToast.s(context, R.string.member_insert_fail_message);
+                            return;
+                        }
+                    }catch (Exception e){
+                        MyToast.s(context,R.string.member_insert_fail_message);
+                        return;
+                    }
+                    currentItem.name = newItem.name;
+                    currentItem.sextype = newItem.sextype;
+                    currentItem.birthday = newItem.birthday;
+                    finish();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+            }
+        });
+    }
+
+    @Override
+    public  void onBackPressed(){
+        close();
+    }
+
+    @Override
+    public void onClick(View v){
+        if(v.getId()==R.id.profile_icon || v.getId() == R.id.profile_icon_change){
+            startProfileIconChange();
+        }
+    }
+
+    private void startProfileIconChange(){
+        Intent intent = new Intent(this,ProfileActivity.class);
+        startActivity(intent);
+    }
+
+
+}
